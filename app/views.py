0d36a528e8072f5f254bbb550efa1c84f4f2e5e4
sjin <sjin@konkuk.ac.kr>
2025-04-04 00:40:44 +0900

    [Chore] Implement view flow

---
+++ app/views.py
@@ -0,0 +1,21 @@
+from app import file_handler
+
+def print_welcome():
+    ## 첫 화면 출력
+    return 
+
+def print_login_or_register():
+    ## 로그인 페이지 출력
+    return
+
+def print_class_list():
+    ## 등록 수업 출력
+    return
+
+def print_member_menu():
+    ## 멤버 출력
+    return 
+
+def print_reservation_history():
+    ## 예약 출력 
+    return 
