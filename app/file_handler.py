65a5efa785d66bd93fb7ddea70e0e021a28ddb16
sjin <sjin@konkuk.ac.kr>
2025-04-04 00:44:33 +0900

    [Chore] Implement file handler flow

---
+++ app/file_handler.py
@@ -0,0 +1,12 @@
+import os
+from app.models import Member, Instructor
+
+MEMBER_FILE = 'data/members.txt'
+INSTRUCTOR_FILE = 'data/instructors.txt'
+RESERVATION_FILE = 'data/reservation.csv'
+
+## 회원 관련 파일 처리
+
+## 강사 관련 파일 처리
+
+## 예약 관련 파일 처리
