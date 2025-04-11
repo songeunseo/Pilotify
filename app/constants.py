e65d9b7987d5728ae42a6740e6c8f3cc0ef7c4fa
sjin <sjin@konkuk.ac.kr>
2025-04-11 15:46:08 +0900

    [Feat] 메인 함수 로직 구현 완료

---
+++ app/constants.py
@@ -0,0 +1,14 @@
+## 성공
+SUCCESS = 0
+
+## 기본 형식 위반
+BASIC_ERROR = -1
+## 조건 위반
+GRAMMAR_ERROR = -2
+## 중복 확인
+NO_DUPLICATION = -3
+
+## Member
+USER_TYPE_MEMBER = 'm'  
+## Instructor    
+USER_TYPE_INSTRUCTOR = 'n'
