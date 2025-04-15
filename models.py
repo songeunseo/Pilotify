6ddfc4cd45081fdca1bc39f9995a15c4b7091839
eunseo <songes04@gmail.com>
2025-04-15 15:31:21 +0900

    refactor: CurrentDateTime 정의 수정

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from datetime import datetime
+from datetime import datetime, date, time
 
 @dataclass
 class Member:
@@ -16,24 +16,22 @@ class Instructor:
     ph : str
 
 class CurrentDateTime:
-    def __init__(self, date_str: str, time_str: str):
+    def __init__(self, datetime_str: str):
         """
-        date_str: 'YYYY-MM-DD'
-        time_str: 'HH:MM'
+        datetime_str: 'YYMMDD,HH:MM'
         """
         # 전체 datetime 문자열로 합쳐서 파싱
-        combined = f"{date_str} {time_str}"
-        self._datetime_obj: datetime = datetime.strptime(combined, "%Y-%m-%d %H:%M")
+        self._datetime_obj: datetime = datetime.strptime(datetime_str, "%y%m%d,%H:%M")
     
     @property
     def datetime_obj(self) -> datetime:
         return self._datetime_obj
     
-    def get_date(self) -> str:
-        return self._datetime_obj.strftime("%Y-%m-%d")
+    def get_date(self) -> date:
+        return self._datetime_obj.date()
     
-    def get_time(self) -> str:
-        return self._datetime_obj.strftime("%H:%M")
+    def get_time(self) -> time:
+        return self._datetime_obj.time()
     
     def __str__(self) -> str:
         return self._datetime_obj.strftime("%Y-%m-%d %H:%M")
