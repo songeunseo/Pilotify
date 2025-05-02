cc3531e9aa87990340289e747a62d7177630cea5
eunseo <songes04@gmail.com>
2025-05-02 21:23:28 +0900

    refactor: validate_datetime_input 리팩토링

--- utils.py
+++ utils.py
@@ -13,51 +13,33 @@ def validate_datetime_input(user_input: str) -> int:
     date_str, time_str = user_input.split(',')
     
     try:
-        # 입력된 날짜 해석
-        input_year = int(date_str[:2])
+        # 입력 날짜,시간 파싱
+        input_year = int(date_str[:2]) + 2000
         input_month = int(date_str[2:4])
         input_day = int(date_str[4:6])
-
-        if 0 <= input_year <= 68:
-            input_full_year = 2000 + input_year
-        else:
-            input_full_year = 2000 + input_year
-
-        input_hour = int(time_str[:2])
-        input_minute = int(time_str[3:5])
-
-        input_datetime = datetime(input_full_year, input_month, input_day, input_hour, input_minute)
+        input_hour, input_minute = map(int, time_str.split(':'))
+        input_datetime = datetime(input_year, input_month, input_day, input_hour, input_minute)
 
         rows = read_csv(DATETIME_PATH)
-        if rows and rows[0]:
-            last_date = rows[0]["datetime"] if isinstance(rows[0], dict) else rows[0][0]
-            last_date_str, last_time_str = last_date.split(',')
-
-            # 저장된 날짜 해석
-            last_year = int(last_date_str[:2])
-            last_month = int(last_date_str[2:4])
-            last_day = int(last_date_str[4:6])
-
-            if 0 <= last_year <= 68:
-                last_full_year = 2000 + last_year
-            else:
-                last_full_year = 2000 + last_year
-
-            last_hour = int(last_time_str[:2])
-            last_minute = int(last_time_str[3:5])
-
-            last_datetime = datetime(last_full_year, last_month, last_day, last_hour, last_minute)
-
-            if input_datetime >= last_datetime:
-                write_csv(DATETIME_PATH, [{"datetime": user_input}])
-                return SUCCESS
-            else:
-                return BASIC_ERROR
-        else:
-            # 파일 비어있으면 그냥 기록
+        # 파일 비어있으면(헤더만 존재) 그냥 기록
+        if len(rows) == 1:
             write_csv(DATETIME_PATH, [{"datetime": user_input}])
             return SUCCESS
 
+        last_date_str, last_time_str = rows[0]["datetime"].split(',')
+        last_year = int(last_date_str[:2]) + 2000
+        last_month = int(last_date_str[2:4])
+        last_day = int(last_date_str[4:6])
+        last_hour, last_minute = map(int, last_time_str.split(':'))
+        last_datetime = datetime(last_year, last_month, last_day, last_hour, last_minute)
+
+        # 입력된 날짜가 마지막 저장 날짜보다 이후인지 확인
+        if input_datetime >= last_datetime:
+            write_csv(DATETIME_PATH, [{"datetime": user_input}])
+            return SUCCESS
+        else:
+            return BASIC_ERROR
+                
     except (ValueError, IndexError, KeyError):
         return BASIC_ERROR
 
@@ -67,6 +49,5 @@ def validate_login_id(id: str, user_list: list):
     for user in user_list:
         if user.id == id:
             return SUCCESS, user
-        #확인
     
     return BASIC_ERROR, None
